57a53008750bb1eb0b49ea5fa68a0ee845032442
kayshwii <kayshwii@icloud.com>
2025-12-28 13:30:41 -0600

    project complete

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 const table = document.getElementById('library-table');
+const newButton = document.getElementById('new-button');
+const submitButton = document.getElementById('submit');
+const form = document.getElementById('book-form');
+let titleForm = document.getElementById('title');
+let authorForm = document.getElementById('author');
+let pagesForm = document.getElementById('pages');
+let readForm = document.getElementById('read');
 
-const myLibrary = [];
+let myLibrary = [];
 
 function Book(title, author, id, pages, read) {
     this.title = title;
@@ -10,21 +17,58 @@ function Book(title, author, id, pages, read) {
     this.id = id;
 }
 
+let renderTable = () => {
+    table.innerHTML = `<table id="library-table"><tr><th>Title</th><th>Author</th><th>Pages</th><th>Read</th><th>Delete</th></tr></table>`;
+    myLibrary.forEach((book) => {
+        const tableRow = document.createElement('tr');
+        const cell = document.createElement('td');
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = 'button';
+        deleteButton.setAttribute('value', book.id);
+        deleteButton.addEventListener('click', e => {
+            removeFromLibrary(deleteButton.value)
+        });
+        tableRow.innerHTML = `<td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.pages}</td>
+            <td class='checkbox' ><input type="checkbox"${book.read ? "checked" : ""}></td>`;
+        tableRow.appendChild(cell);
+        cell.appendChild(deleteButton)
+        table.appendChild(tableRow);
+    });
+    console.log(myLibrary);
+}
+
 let addBookToLibrary = (title, author, pages, read) => {
     let id = crypto.randomUUID();
     const book = new Book(title, author, id, pages, read);
     myLibrary.push(book);
 }
 
+let removeFromLibrary = (id) => {
+    myLibrary = myLibrary.filter((book) => book.id !== id);
+    renderTable();
+}
+
+newButton.addEventListener("click", e => {
+    form.style.display = 'block';
+    newButton.style.display = 'none';
+});
+
+submitButton.addEventListener("click", e => {
+    e.preventDefault();
+    form.style.display = 'none';
+    newButton.style.display = 'block';
+    addBookToLibrary(titleForm.value, authorForm.value, pagesForm.value, readForm.checked);
+    titleForm.value = '';
+    authorForm.value = '';
+    pagesForm.value = '';
+    readForm.checked = false;
+    renderTable();
+})
+
+
 addBookToLibrary('Queen of the Damned', 'Anne Rice', 480, true);
 addBookToLibrary('Interview with a Vampire', 'Anne Rice', 369, true);
-addBookToLibrary('The Vampire Lestat', 'Anne Rice', 481, true);
-console.log(myLibrary);
-myLibrary.forEach((book) => {
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `<td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.pages}</td>
-        <td>${book.read}</td>`;
-    table.appendChild(tableRow)
-})
+addBookToLibrary('The Vampire Lestat', 'Anne Rice', 481, false);
+renderTable();
